42104c9791095f0e3a6ac78ea47dfe404e9c10cc
Moa <moali02@yahoo.se>
2023-11-08 22:15:28 +0100

    Co-authored-by: Emil0824 <Emil0824@users.noreply.github.com>

---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:jobbagregator/widgets/navigation_bar/navigation_bar.dart';
+
+class HomeView extends StatelessWidget {
+    const HomeView({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.red,
+            body: Column(children: <Widget>[
+                Navbar()
+            ],),
+        );
+    }
+}
