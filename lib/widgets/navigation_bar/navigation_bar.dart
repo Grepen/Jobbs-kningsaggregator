70fa24729baa45ea16e73a5fcada43e7af17e6f8
Moa <moali02@yahoo.se>
2023-11-11 20:05:07 +0100

    Co-authored-by: Emil0824 <Emil0824@users.noreply.github.com>

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jobbagregator/widgets/search_bar/search_bar.dart';
+
 
 class Navbar extends StatelessWidget {
     const Navbar({Key? key}) : super(key: key);
@@ -6,20 +8,14 @@ class Navbar extends StatelessWidget {
     @override
     Widget build(BuildContext context){
         return Container(
+            color: Color(0xFFDAD5D1),
             height: 100,
             child: Row(
                 children: <Widget>[
-                    SizedBox(
-                        height: 80,
-                        width: 150,
-                        child: Image.asset('assets/logo.png'),
-                    ), //SizedBox
                     Row(
                       mainAxisSize: MainAxisSize.min, 
                       children: <Widget>[
-                        _NavBarItem('Episodes'),
-                        SizedBox(width: 60,),
-                        _NavBarItem('About')
+                        SearchBarApp()
                       ],
                     )
                 ],
